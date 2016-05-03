63c04ad7a7adb75f807a03f5632c1721724b4707
David Ginzbourg <davidginzbourg@gmail.com>
2016-05-03 22:10:07 +0300

    Rename user json to user yaml

--- spuc/services/google.py
+++ spuc/services/google.py
@@ -2,10 +2,10 @@ import httplib2
 from googleapiclient import discovery
 
 
-def create_user(user_json, credentials):
+def create_user(user_yaml, credentials):
 
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('admin', 'directory_v1', http=http)
 
-    result = service.users().insert(body=user_json).execute()
+    result = service.users().insert(body=user_yaml).execute()
     return result
