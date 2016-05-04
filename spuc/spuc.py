0b4992545bfb8a5f6d261f97d55013eba59f2bad
David Ginzbourg <davidginzbourg@gmail.com>
2016-05-04 22:39:48 +0300

    Move methods to utils

--- spuc/spuc.py
+++ spuc/spuc.py
@@ -16,60 +16,6 @@ class Spuc(object):
                 credential_config_path)
         self.user_config_dict = self.convert_file_to_yaml(user_config_path)
 
-    def get_oauth_credentials(self, credential_config_dict,
-                              scopes, name_prefix):
-        """Gets valid user credentials from storage.
-
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-
-        Returns:
-            Credentials, the obtained credential.
-        """
-        oauth_credential_path = self.create_credential_json(
-                credential_config_dict)
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(
-                credential_dir,
-                '{0}_credentials.json'.format(name_prefix)
-        )
-
-        store = file.Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(
-                    oauth_credential_path,
-                    scopes
-            )
-            flow.user_agent = APPLICATION_NAME
-            flags = tools.argparser.parse_args(args=[])
-            credentials = tools.run_flow(flow, store, flags)
-            print('Storing credentials to ' + credential_path)
-        return credentials
-
-    def create_in_google(self):
-        credentials = self.get_oauth_credentials(
-                credential_config_dict=self.credential_config_dict['gapps'],
-                scopes=GOOGLE_SCOPES,
-                name_prefix='google'
-        )
-
-        print google.create_user(self.user_config_dict['gapps'],
-                credentials
-        )
-
-    def create_in_aws(self):
-        pass
-
-    def create_in_github(self):
-        pass
-
-    def create_in_jira(self):
-        pass
-
     def convert_file_to_yaml(self, yaml_file_path):
         with open(str(yaml_file_path)) as yaml_file:
             return yaml.load(yaml_file)
