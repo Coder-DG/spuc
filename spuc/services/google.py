e3d30ecf9809add784a04fde826f59db788de0bb
David Ginzbourg <davidginzbourg@gmail.com>
2016-05-04 22:57:41 +0300

    Move click command to google module

--- spuc/services/google.py
+++ spuc/services/google.py
@@ -1,11 +1,33 @@
+import click
+
 import httplib2
 from googleapiclient import discovery
 
 
 def create_user(user_yaml, credentials):
-
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('admin', 'directory_v1', http=http)
 
     result = service.users().insert(body=user_yaml).execute()
     return result
+
+
+@click.group()
+def gapps():
+    pass
+
+
+@gapps.command(name='create')
+@click.option('-c', '--credential-config-path',
+              help='The path to admin credential file.',
+              required=True)
+@click.option('-u', '--user-yaml-path',
+              help='The path to the user yaml config file.',
+              required=True)
+def create_user_google(credential_config_path, user_yaml_path):
+    user_google = Spuc(
+            credential_config_path,
+            user_yaml_path
+    )
+
+    user_google.create_in_google()
