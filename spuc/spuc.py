27d7af2a85dbf50990d7b2b70fe9c665b118c1ac
David Ginzbourg <davidginzbourg@gmail.com>
2016-05-04 22:58:10 +0300

    Use utils command

--- spuc/spuc.py
+++ spuc/spuc.py
@@ -8,13 +8,9 @@ GOOGLE_SCOPES = 'https://www.googleapis.com/auth/admin.directory.user'
 
 class Spuc(object):
     def __init__(self, credential_config_path, user_config_path):
-        self.credential_config_dict = self.convert_file_to_yaml(
+        self.credential_config_dict = utils.convert_file_to_yaml(
                 credential_config_path)
-        self.user_config_dict = self.convert_file_to_yaml(user_config_path)
-
-    def convert_file_to_yaml(self, yaml_file_path):
-        with open(str(yaml_file_path)) as yaml_file:
-            return yaml.load(yaml_file)
+        self.user_config_dict = utils.convert_file_to_yaml(user_config_path)
 
     def create_all(self):
         credentials = utils.get_oauth_credentials(
