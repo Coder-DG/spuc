69f0949b6126b19bfb60ea36d4e724b40a915740
David Ginzbourg <davidginzbourg@gmail.com>
2016-05-05 15:32:17 +0300

    Move gapps click command back to main module

--- spuc/services/google.py
+++ spuc/services/google.py
@@ -1,9 +1,5 @@
-import click
-
 import httplib2
 from googleapiclient import discovery
-
-from spuc import utils
 
 
 def create_user(user_yaml, credentials):
@@ -12,26 +8,3 @@ def create_user(user_yaml, credentials):
 
     result = service.users().insert(body=user_yaml).execute()
     return result
-
-
-@click.group()
-def gapps():
-    pass
-
-
-@gapps.command(name='create')
-@click.option('-c', '--credential-config-path',
-              help='The path to admin credential file.',
-              required=True)
-@click.option('-u', '--user-yaml-path',
-              help='The path to the user yaml config file.',
-              required=True)
-def create_user_google(credential_config_path, user_yaml_path):
-    create_user(
-            utils.convert_file_to_yaml(user_yaml_path)['gapps'],
-            utils.get_oauth_credentials(
-                    utils.convert_file_to_yamlcredential_config_path)[
-                'gapps'],
-            utils.GOOGLE_SCOPES,
-            'google'
-    )
--- spuc/spuc.py
+++ spuc/spuc.py
@@ -25,3 +25,26 @@ class Spuc(object):
 @click.group()
 def main():
     pass
+
+
+@main.group()
+def gapps():
+    pass
+
+
+@gapps.command(name='create')
+@click.option('-c', '--credential-config-path',
+              help='The path to admin credential file.',
+              required=True)
+@click.option('-u', '--user-yaml-path',
+              help='The path to the user yaml config file.',
+              required=True)
+def create_user_google(credential_config_path, user_yaml_path):
+    google.create_user(
+            utils.convert_file_to_yaml(user_yaml_path)['gapps'],
+            utils.get_oauth_credentials(
+                    utils.convert_file_to_yamlcredential_config_path)[
+                'gapps'],
+            utils.GOOGLE_SCOPES,
+            'google'
+    )
