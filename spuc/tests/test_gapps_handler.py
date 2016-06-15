cf9db2ae4f2db02c6db7c9fbd7cc62efb0b44a3e
David Ginzbourg <davidginzbourg@gmail.com>
2016-06-15 17:18:20 +0300

    Add none user config test

--- spuc/tests/test_gapps_handler.py
+++ spuc/tests/test_gapps_handler.py
@@ -1,6 +1,14 @@
 import unittest
 
+from spuc import utils
+from spuc.services import github_handler
+
 
 class GappsHandlerCase(unittest.TestCase):
-    def test_no_user_config(self):
-        pass
+    def test_none_user_config(self):
+        self.assertRaises(
+                utils.SpucException,
+                github_handler.invite_user,
+                user_config=None,
+                service_config=''
+        )
