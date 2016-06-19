2250031dbdcaa025027eb159ed953db11d65d1ae
David Ginzbourg <davidginzbourg@gmail.com>
2016-06-19 14:06:13 +0300

    Add missing service config key

--- spuc/tests/test_github_handler.py
+++ spuc/tests/test_github_handler.py
@@ -1,5 +1,7 @@
 import unittest
 
+import mock
+
 from spuc import utils
 from spuc.services import github_handler
 
@@ -20,3 +22,22 @@ class GitHubHandlerCase(unittest.TestCase):
                 user_config='',
                 service_config=None
         )
+
+    @mock.patch('utils.convert_config_file')
+    def test_missing_user_config_key(self, mock_convert_config_file_util):
+        mock_convert_config_file_util.return_value = \
+            self.return_bad_service_config
+
+        self.assertRaises(
+                utils.SpucException,
+                github_handler.invite_user(
+                        user_config='',
+                        service_config='service_config'
+                )
+        )
+
+    def return_bad_service_config(self, config):
+        if config == 'service_config':
+            return {}
+
+        return {'github': ''}
